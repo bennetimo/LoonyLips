ba8338b214147c5f42bf9f3f30a28c4e6adb0c50
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2019-08-12 20:03:53 +0100

    add initial game files

---
+++ LoonyLips.gd
@@ -0,0 +1,48 @@
+extends Control
+
+var player_words = []
+var prompts = ["a name", "a noun", "adverb", "adjective"]
+var story = "Once upon a time %s watched %s with %s and thought it was %s"
+
+onready var PlayerText = $"VBoxContainer/HBoxContainer/PlayerText"
+onready var DisplayText = $"VBoxContainer/DisplayText"
+
+func _ready():
+	DisplayText.text = "Welcome to Loony Lips! "
+	PlayerText.grab_focus()
+	check_player_words_length()
+
+func _on_PlayerText_text_entered(new_text):
+	add_to_player_words()
+
+func _on_TextureButton_pressed():
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
+
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
+
+func is_story_done():
+	return player_words.size() == prompts.size()
+	
+func check_player_words_length():
+	if is_story_done():
+		end_game()
+	else:
+		prompt_player()
+		
+func tell_story():
+	DisplayText.text = story % player_words
+	
+func prompt_player():
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
+	
+func end_game():
+	PlayerText.queue_free()
+	$VBoxContainer/HBoxContainer/Label.text = "Again!"
+	tell_story()
